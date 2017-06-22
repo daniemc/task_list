a7ffb21e3c3f89e537fb62f39db5a7803917895c
MATTELSA\dmenesesc <danielmeneses@mattelsa.net>
2017-06-22 14:29:54 -0500

    REFACTOR - add component task

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
+Vue.component('app-task', {
+    template: '#task-template',
+    props: ['tasks', 'task', 'index'],
+    methods: {
+        updateStatus: function(){
+            this.task.pending = !this.task.pending;
+        },
+        edit: function(){
+            this.tasks.forEach(function(task) {
+                task.editing = false;
+            });
+            this.edited_description = this.task.description;
+            this.task.editing = true;
+        },
+        update: function(){
+            this.task.description = this.edited_description;
+            this.task.editing = false;
+        },
+        discartEditing: function(){
+            this.task.editing = false;
+        },
+        remove: function(){
+            this.tasks.splice(this.index, 1);
+        },
+    }
+})
+
 var vm = new Vue({
     el: "#app",
     methods: {
@@ -9,26 +36,8 @@ var vm = new Vue({
             });
             this.new_task = "";
         },
-        updateStatus: function(task){
-            task.pending = !task.pending;
-        },
-        editTask: function(task){
-            this.tasks.forEach(function(task) {
-                task.editing = false;
-            });
-            this.edited_description = task.description;
-            task.editing = true;
-        },
-        updateTask: function(task){
-            task.description = this.edited_description;
-            task.editing = false;
-        },
-        discartEditing: function(task){
-            task.editing = false;
-        },
-        removeTask: function(index){
-            this.tasks.splice(index, 1);
-        },
+        
+        
         removeCompleteTasks: function(){
             this.tasks = this.tasks.filter(function(task){
                 return task.pending;
